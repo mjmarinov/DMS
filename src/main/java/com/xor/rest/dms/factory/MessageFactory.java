825b1abbbca86060859a125f2e1cf8607dc2ff1d
Miroslav Marinov <m.j.marinov@gmail.com>
2018-08-20 13:19:40 +0300

    Removing the enumeration

--- src/main/java/com/xor/rest/dms/factory/MessageFactory.java
+++ src/main/java/com/xor/rest/dms/factory/MessageFactory.java
@@ -5,41 +5,24 @@ import org.springframework.stereotype.Component;
 import com.xor.rest.dms.exception.DMSBusinessException;
 import com.xor.rest.dms.model.AbstractMessage;
 import com.xor.rest.dms.model.EmotionMessage;
-import com.xor.rest.dms.model.MessageTypeEnum;
 import com.xor.rest.dms.model.TextMessage;
 
 @Component
 public class MessageFactory {
 	
 	public AbstractMessage create(String messageType) {
+		
 		if (messageType == null)
 			throw new DMSBusinessException("Message type cannot be null!");
-		
-		MessageTypeEnum mt = null;
-		
-		try {
-			mt = MessageTypeEnum.valueOf(messageType.toUpperCase()); 
-		} catch (IllegalArgumentException ex) {
+
+		switch (messageType.toLowerCase()) {
+		case "send_text":
+			return new TextMessage();
+		case "send_emotion":
+			return new EmotionMessage();
+		default:
 			throw new DMSBusinessException("Invalid message type: " + String.valueOf(messageType));
 		}
-		
-		return create(mt);
 	}	
 
-	public AbstractMessage create(MessageTypeEnum messageType) {
-		
-		if (messageType == null) {
-			throw new DMSBusinessException("Message type cannot be null!");
-		}
-		
-		switch (messageType) {
-		case SEND_TEXT:
-			return new TextMessage();
-		case SEND_EMOTION:
-			return new EmotionMessage();
-		default: 
-			return null;
-		}
-	}	
-	
 }
